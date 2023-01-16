141d5b09abacea0e1686d2a8ac7c3b7b6d01f400
Yushun Zeng <d18130495@mytudublin.ie>
2023-01-16 16:30:24 +0000

    Updated login expiry handle

---
+++ model/src/main/java/com/yushun/recommender/vo/user/user/UserDetailReturnVo.java
@@ -0,0 +1,47 @@
+package com.yushun.recommender.vo.user.user;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+
+/**
+ * <p>
+ * User Detail Result Return Vo
+ * </p>
+ *
+ * @author yushun zeng
+ * @since 2023-1-16
+ */
+
+public class UserDetailReturnVo {
+    @TableField("email")
+    private String email;
+
+    @TableField("username")
+    private String username;
+
+    @TableField("avatar")
+    private String avatar;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
--- service/src/main/java/com/yushun/recommender/controller/user/UserInterfaceUserDetailController.java
+++ service/src/main/java/com/yushun/recommender/controller/user/UserInterfaceUserDetailController.java
@@ -1,22 +1,50 @@
 package com.yushun.recommender.controller.user;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yushun.recommender.model.common.User;
 import com.yushun.recommender.security.result.Result;
-import com.yushun.recommender.security.user.UserRepository;
 import com.yushun.recommender.security.utils.JwtTokenProvider;
 import com.yushun.recommender.service.user.UserInterfaceUserService;
+import com.yushun.recommender.vo.user.user.UserDetailReturnVo;
+import com.yushun.recommender.vo.user.user.UserReturnVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/user/detail")
+@RequestMapping("/user")
 public class UserInterfaceUserDetailController {
     @Autowired
     private UserInterfaceUserService userInterfaceUserService;
 
-    @GetMapping("/")
-    public Result test(String email) {
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/getUserDetailByToken")
+    public Result getUserDetailByToken(String token) {
+        String userEmail = jwtTokenProvider.getUsername(token);
+
+        // find user
+        QueryWrapper userWrapper = new QueryWrapper();
+        userWrapper.eq("email", userEmail);
+
+        User findUser = userInterfaceUserService.getOne(userWrapper);
+
+        if(findUser == null) {
+            return Result.fail().message("Can not find user");
+        }
+
+        // form return data
+        UserDetailReturnVo userDetailReturnVo = new UserDetailReturnVo();
+        BeanUtils.copyProperties(findUser, userDetailReturnVo);
+
+        return Result.ok(userDetailReturnVo).message("");
+    }
+
+
+    @GetMapping("/getUserDetailByEmail")
+    public Result getUserDetailByEmail(String email) {
         // TODO
         System.out.println(email);
 
